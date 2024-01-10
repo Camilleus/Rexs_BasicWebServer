55530d6a89315674ab68ad9c87848e7dbc64ac7e
CamilleusRex <camilleusrex@gmail.com>
2024-01-10 18:45:18 +0100

    Update server.py

--- server.py
+++ server.py
@@ -1,14 +1,42 @@
 import asyncio
+import logging
 import websockets
+from websockets import WebSocketServerProtocol
+from websockets.exceptions import ConnectionClosedOK
 
-async def handler(websocket, path):
-    data = await websocket.recv()
-    reply = f"Data recieved as:  {data}!"
-    print(reply)
-    await websocket.send(reply)
+logging.basicConfig(level=logging.INFO)
+
+class Server:
+    clients = set()
+
+    async def register(self, ws: WebSocketServerProtocol):
+        self.clients.add(ws)
+        logging.info(f'{ws.remote_address} connects')
+
+    async def unregister(self, ws: WebSocketServerProtocol):
+        self.clients.remove(ws)
+        logging.info(f'{ws.remote_address} disconnects')
+
+    async def send_to_clients(self, message: str):
+        if self.clients:
+            [await client.send(message) for client in self.clients]
+
+    async def ws_handler(self, ws: WebSocketServerProtocol):
+        await self.register(ws)
+        try:
+            await self.distrubute(ws)
+        except ConnectionClosedOK:
+            pass
+        finally:
+            await self.unregister(ws)
+
+    async def distrubute(self, ws: WebSocketServerProtocol):
+        async for message in ws:
+            await self.send_to_clients(message)
 
 async def main():
-    async with websockets.serve(handler, "localhost", 8000):
+    server = Server()
+    async with websockets.serve(server.ws_handler, 'localhost', 4000):
         await asyncio.Future()# run forever
 
 if __name__ == '__main__':
